407fcabefdbb020749c1ad309463dbc15faa21c6
ArtemFedorchuk <fedorchuk.zver@gmail.com>
2023-06-17 22:36:58 +0300

    Minor improvements

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,31 +1,29 @@
-import React from 'react';
+import { useState } from 'react';
 import styles from './Card.module.scss';
 
-function Card(props) {
-    const [isAdded, setIsAdded] = React.useState(false);
+function Card({ onClickFavorite, imageUrl, title, price }) {
+    const [isAdded, setIsAdded] = useState(false);
 
-    const onClickPlus = () => {
-        setIsAdded(true);
-    }
-
-    
     return (
         <div className={styles.card}>
-
-            <div className={styles.favorite} onClick={props.onClickFavorite}>
-                <img src="/img/heart-unliked.svg" alt="Unliked" />
+            <div className={styles.favorite} onClick={onClickFavorite}>
+                <img src="/img/heart-unliked.svg" alt="unliked" />
             </div>
-            
-            <img src={props.imageUrl} style={{ width: 133, height: 112 }} alt="Sneakers" />
-            <h5>{props.title}</h5>
+
+            <img src={imageUrl} style={{ width: 133, height: 112 }} alt="sneakers" />
+            <h5>{title}</h5>
             <div className="d-flex justify-between align-center">
                 <div className="d-flex flex-column">
                     <span>Ціна:</span>
-                    <b>{props.price} грн.</b>
+                    <b>{price} грн.</b>
                 </div>
-                <img className={styles.plus} onClick={onClickPlus}  src={isAdded ? "/img/btn-checked.svg" : "/img/btn-plus.svg"} alt="Plus" />  
+                <img
+                    className={styles.plus}
+                    onClick={() => setIsAdded(!isAdded)}
+                    src={isAdded ? "/img/btn-checked.svg" : "/img/btn-plus.svg"}
+                    alt="plus"
+                />
             </div>
-            
         </div>
     );
 }
