ab4697a364ffa23d5a24d56c4a10b7fb6d59230f
IhorShylov <shilov0209@gmail.com>
2023-06-18 19:17:13 +0300

    added functional on buttons

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import styles from './Card.module.scss';
 
-function Card(props) {
+function Card({imageUrl, title, price, onPlus, onFavorite}) {
     const [isAdded, setIsAdded] = React.useState(false);
 
     const onClickPlus = () => {
-        setIsAdded(true);
+        onPlus();
+        setIsAdded(!isAdded);
     }
 
     const [isAddedFavorite, setIsAddedFavorite] = React.useState(false);
 
     const onClickFavorite = () => {
-        setIsAddedFavorite(true);
+        onFavorite();
+        setIsAddedFavorite(!isAddedFavorite);
     }
 
     return (
@@ -21,12 +23,12 @@ function Card(props) {
                 <img src={isAddedFavorite ? "/img/heart-liked.svg" : "/img/heart-unliked.svg"} alt="Unliked" />
             </div>
             
-            <img src={props.imageUrl} style={{ width: 133, height: 112 }} alt="Sneakers" />
-            <h5>{props.title}</h5>
+            <img src={imageUrl} style={{ width: 133, height: 112 }} alt="Sneakers" />
+            <h5>{title}</h5>
             <div className="d-flex justify-between align-center">
                 <div className="d-flex flex-column">
                     <span>Ціна:</span>
-                    <b>{props.price} грн.</b>
+                    <b>{price} грн.</b>
                 </div>
                 <img className={styles.plus} onClick={onClickPlus}  src={isAdded ? "/img/btn-checked.svg" : "/img/btn-plus.svg"} alt="Plus" />  
             </div>
--- src/components/Drawer.js
+++ src/components/Drawer.js
@@ -1,53 +1,45 @@
 
-function Drawer() {
-    return (
-      <div style={{display: 'none'}} className="overlay">
+function Drawer({onClose, items = []}) {
+  return (
+    <div className="overlay">
 
-        <div className="drawer">
+      <div className="drawer">
 
-          <h2 className="mb-30 d-flex justify-between">Кошик <img className="removeBtn cu-p" src="/img/btn-remove.svg" alt="Remove" /></h2>
+        <h2 className="mb-30 d-flex justify-between">Кошик <img className="removeBtn cu-p" 
+        onClick={onClose} src="/img/btn-remove.svg" alt="Close" /></h2>
 
-          <div className="items">
-
+        <div className="items">
+          {items.map((obj) => (
             <div className="cartItem d-flex align-center mb-20">
-              <div style={{backgroundImage: 'url(/img/sneakers/1.jpg)'}} className="cartItemImg"> </div>
+              <div style={{ backgroundImage: `url(${obj.imageUrl})` }} className="cartItemImg"> </div>
               <div className="mr-20 flex">
-                <p className="mb-5">Чоловічі кросівки Nike Blazer Mid Suede</p>
-                <b>2999 грн.</b>
+                <p className="mb-5">{obj.title}</p>
+                <b>{obj.price} грн.</b>
               </div>
               <img className="removeBtn" src="/img/btn-remove.svg" alt="Remove" />
             </div>
-
-            <div className="cartItem d-flex align-center mb-20">
-              <div style={{backgroundImage: 'url(/img/sneakers/2.jpg)'}} className="cartItemImg"> </div>
-              <div className="mr-20 flex">
-                <p className="mb-5">Чоловічі кросівки Nike Air Max 270</p>
-                <b>2459 грн.</b>
-              </div>
-              <img className="removeBtn" src="/img/btn-remove.svg" alt="Remove" />
-            </div>
-
-          </div>
-
-          <div className="cartTotalBlock">
-            <ul>
-                <li>
-                  <span>Разом:</span>
-                  <div></div>
-                  <b>5458 грн.</b>
-                </li>
-                <li>
-                  <span>Податок 5%:</span>
-                  <div></div>
-                  <b>272.90 грн.</b>
-                </li>
-            </ul>
-            <button className="greenButton">Оформити замовлення <img src="/img/arrow.svg" alt="Arrow" /></button>
-          </div> 
-
+          ))}
         </div>
+
+        <div className="cartTotalBlock">
+          <ul>
+            <li>
+              <span>Разом:</span>
+              <div></div>
+              <b>5458 грн.</b>
+            </li>
+            <li>
+              <span>Податок 5%:</span>
+              <div></div>
+              <b>272.90 грн.</b>
+            </li>
+          </ul>
+          <button className="greenButton">Оформити замовлення <img src="/img/arrow.svg" alt="Arrow" /></button>
+        </div>
+
       </div>
-    );
+    </div>
+  );
 }
 
 export default Drawer;
