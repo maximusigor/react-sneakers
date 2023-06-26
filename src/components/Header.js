56264a9e702ddc58c82667fb3eacfc4c91658b03
IhorShylov <shilov0209@gmail.com>
2023-06-26 22:10:50 +0300

    added links

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,6 +4,8 @@ import { Link } from "react-router-dom";
 function Header(props) {
     return (
         <header className="d-flex justify-between align-center p-40">
+            
+            <Link to="/favorites">
             <div className="d-flex align-center">
             <img width = {40} height={40} src='/img/logo.png' alt="logotype" />
             <div>
@@ -11,13 +13,15 @@ function Header(props) {
                 <p className="opacity-5">Магазин найкращих кросівок</p>
             </div>
             </div>
+            </Link>
+
             <ul className="d-flex">
                 <li onClick={props.onClickCart} className="mr-30 cu-p">
                     <img width = {18} height={18} src='/img/cart.svg' alt="cart" />
                     <span>735грн.</span>
                 </li>
                 <li>
-                    <Link>
+                    <Link to="/favorites">
                         <img width = {18} height={18} src='/img/heart.svg' alt="bookmarks" />
                     </Link>
                 </li>
